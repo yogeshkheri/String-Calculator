058494f10d71e2e8cbd2a3a136c984b0099eca9b
Yogesh <yogeshkheri@gmail.com>
2021-02-15 21:31:45 +0530

    First Commit

---
+++ Test/src/TestForTestCAses/TestCase.java
@@ -0,0 +1,9 @@
+package TestForTestCAses;
+
+public class TestCase {
+
+	public static int  add(int val)
+	{
+		return val;
+	}
+}
---
+++ Test/src/TestForTestCAses/Testing.java
@@ -0,0 +1,15 @@
+package TestForTestCAses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test() {
+		
+		assertEquals(0, TestCase.add(0));
+	}
+
+}
