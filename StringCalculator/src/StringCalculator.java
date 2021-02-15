ec014d9760c78fd491b82f012811a770bd264bca
Yogesh <yogeshkheri@gmail.com>
2021-02-15 21:56:47 +0530

    Second Commit

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -2,6 +2,17 @@
 public class StringCalculator {
 
 	public static int add(String val) {
-		return 0;
+		
+		if(val.isEmpty())
+		{
+		return 0; // Return Value If String Is Empty
+		}
+		else if (val.contains(",")) { // Return Sum of Two Values Only If they Contains comma and Return Sum of Values 
+			String[] tokenArray = val.split(",");
+			return Integer.parseInt(tokenArray[0])+ Integer.parseInt(tokenArray[1]);
+		}
+		else {
+			return Integer.parseInt(val); // Return Value If String have only Single Value
+		}
 	}
 }
--- StringCalculator/src/StringCalculatorTest.java
+++ StringCalculator/src/StringCalculatorTest.java
@@ -8,5 +8,13 @@ public class StringCalculatorTest {
 	public void EmptyStringReturnZero() { // This Test Case For Return Zero, If String Is Empty
 		assertEquals(0, StringCalculator.add(""));
 	}
-
+	@Test
+	public void StringWithOneReturnValue() { // This Test Case For Sending 1 as String and Return Value is 1
+		assertEquals(1,StringCalculator.add("1"));
+	}
+	@Test
+	public void StringWithOneAndTwoReturnSumValue() {
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
+	
 }
